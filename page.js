b0e60ab1bb4522220c7a2dcfb3831505ea5ca480
cpsoucy <96217410+cpsoucy@users.noreply.github.com>
2023-03-26 20:11:38 -0400

    adding navigable file to website for testing

--- page.js
+++ page.js
@@ -1,1 +1,19 @@
-//random.jpg
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  const formData = new FormData(this);
+  
+  fetch('/upload', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    // handle the response from the server
+  })
+  .catch(error => {
+    console.error(error);
+  });
+});
